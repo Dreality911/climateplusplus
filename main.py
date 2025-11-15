04ca2edb696f120f4f9febfba6fb62ae69987cd4
FloCode2010 <108221218+FloCode2010@users.noreply.github.com>
2025-11-15 14:05:08 +0100

    removed some stuff from `main.py`

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 rcp = None                  # representative concentration pathway 
 ggc = None                  # Treibhausgaskonzentration (ppm CO2-Äquivalent)
-tempGain = None             # Temperaturerhöhung (°C)               Link: https://climate.nasa.gov/vital-signs/global-temperature/  & Link: https://de.statista.com/statistik/daten/studie/1073559/umfrage/durchschnittliche-temperaturschwankungen-land-meer/#:~:text=Die%20Temperaturen%20des%20Jahres%201850%20lagen%20durchschnittlich%20rund,weltweit%20das%20w%C3%A4rmste%20Jahr%20seit%20Beginn%20der%20Aufzeichnungen.  & Link: https://www.climate.gov/news-features/understanding-climate/climate-change-global-temperature
+globalTemp = None           # globale Temperatur (°C)               Link: https://climate.nasa.gov/vital-signs/global-temperature/  & Link: https://de.statista.com/statistik/daten/studie/1073559/umfrage/durchschnittliche-temperaturschwankungen-land-meer/#:~:text=Die%20Temperaturen%20des%20Jahres%201850%20lagen%20durchschnittlich%20rund,weltweit%20das%20w%C3%A4rmste%20Jahr%20seit%20Beginn%20der%20Aufzeichnungen.  & Link: https://www.climate.gov/news-features/understanding-climate/climate-change-global-temperature
 snowCover = None            # Eisfläche (km²)
 avgPrec = None              # Niederschlag (mm/Jahr)                Link: 
 gsStrenght = None           # Golfstromstärke (Sv)
@@ -12,5 +12,3 @@ seaLevel = None             # Meeresspiegel (m)                     Link: https:
 forestArea = None           # Waldfläche (km²)                      Link: https://www.fao.org/3/i4793e/i4793e.pdf
 winterLength = None         # Winterlänge (Tage)                    
 permafrost = None           # Permafrost (km²)
-bioflora = None             # Biologische Vielfalt (Anzahl Arten) 
-livestockBreeding = None    # Nutztierhaltung (Anzahl Tiere)         
